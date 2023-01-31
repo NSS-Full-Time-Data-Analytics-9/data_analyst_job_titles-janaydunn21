d7392f824e5b8acbadae7b9cc321983a0a897c20
JaNay Dunn <49759196+janaydunn21@users.noreply.github.com>
2023-01-31 15:54:18 -0600

    Tuesday Afternoon push

--- Project_Data_Analyst_Job_Title(1).sql
+++ Project_Data_Analyst_Job_Title(1).sql
@@ -31,6 +31,59 @@ WHERE review_count BETWEEN 500 AND 1000;
 --answer: 151
 
 --6.
-SELECT location GROUP BY location AS state, AVG(star_rating) AS avg_rating
+SELECT location AS state, AVG(star_rating) AS average_rating
+FROM data_analyst_jobs
+GROUP BY location
+ORDER BY AVG(star_rating)DESC;
+--answer: NE
+
+--7. Select unique job titles from the data_analyst_jobs table. How many are there?
+SELECT COUNT(DISTINCT title) 
 FROM data_analyst_jobs;
+--answer: 881
+
+--8. How many unique job titles are there for California companies?
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE location= 'CA';
+--answer: 230
+
+--9. Find the name of each company?
+SELECT company, AVG(star_rating)
+FROM data_analyst_jobs
+WHERE review_count > 5000 AND company IS NOT NULL 
+GROUP BY company;
+--answer: 40
+
+--10. What is that rating?
+SELECT company, AVG(star_rating)
+FROM data_analyst_jobs
+WHERE review_count > 5000
+GROUP BY company
+ORDER by AVG(star_rating) DESC;
+--General Motors, Unilever, Microsoft, Nike (rating 4.1)
+
+--11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT COUNT (DISTINCT title)
+FROM data_analyst_jobs
+WHERE title ILIKE '%Analyst%';
+--774
+
+--12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+SELECT DISTINCT title 
+FROM data_analyst_jobs
+WHERE title NOT ILIKE '%Analyst%' AND title NOT ILIKE '%Analytics%';
+-- 4
+
+--BONUS-
+SELECT COUNT(title), domain
+FROM data_analyst_jobs
+WHERE skill ILIKE '%SQL%' 
+AND domain IS NOT NULL
+AND days_since_posting > 21
+GROUP BY domain
+ORDER BY COUNT(title) DESC
+LIMIT 4;
+--Internet and Software, Banks and Financial Services, Consulting and Business Services, Health Care 
+
 
